6236d7cedcb91d1d3f1c91f39bafe63a02f58050
karl oficina <karl@aspire.com>
2019-06-17 01:31:32 -0400

    indica al driver que envie un dato cuando se apreta el boton

--- main.py
+++ main.py
@@ -67,6 +67,9 @@ class Driver():
         print("inicializando")
         self.recv_stream, self.send_stream = get_socket_stream('ESP32test')
         self.input_thread = myThread(1, "Thread-1", 1, self.recv_stream)
+    def enviar(self):
+        print("ENVIANDO...")
+        self.send_stream.write("{\"datum\":{\"potencia\":99.9}}\n".encode())
 
 class DataloggerMain(Widget):
     random_number = StringProperty()
@@ -75,6 +78,7 @@ class DataloggerMain(Widget):
     def mensaje(self, evento):
         self.random_number = "WENA"
         print(self.bt_driver)
+        self.bt_driver.enviar()
         print("CLICK")
         print(evento)
     #def on_touch_down(self, argumento):
