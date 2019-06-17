5104bc1699adba7eedf86c04e22b470f7b6bdcfb
karl oficina <karl@aspire.com>
2019-06-16 22:16:26 -0400

    creado un thread para manejar la recepción de datos

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 
+import threading
+import time
+
 # Same as before, with a kivy-based UI
 
 '''
@@ -21,6 +24,49 @@ BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
 BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
 BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
 UUID = autoclass('java.util.UUID')
+
+exit_flag = 0
+
+def print_time(threadName, delay, counter):
+    while counter:
+        #if exitFlag:
+        #    threadName.exit()
+        time.sleep(delay)
+        print ("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter, inputStream):
+        threading.Thread.__init__(self)
+        self.inputStream = inputStream
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print ("Starting " + self.name)
+        counter = 5
+        mensaje = ""
+        while counter:
+            if (self.inputStream.available()):
+                input_chr = chr(self.inputStream.read())
+                mensaje = mensaje + input_chr
+                if (input_chr == '\n'):
+                    print(mensaje)
+                    mensaje = ""
+            '''
+            data_in = self.inputStream.available()
+            buffer_array = []
+            if (data_in):
+                byte_string = self.inputStream.read(buffer_array)
+                print(byte_string)
+                print(buffer_array)
+                data_in = 0
+                #print(byte_string.decode())
+            #print(self.inputStream)
+            '''
+        print_time(self.name, self.counter, 5)
+        print ("Exiting " + self.name)
+
 
 
 def get_socket_stream(name):
@@ -74,14 +120,17 @@ BoxLayout:
         def build(self):
             #self.recv_stream, self.send_stream = get_socket_stream('linvor')
             self.recv_stream, self.send_stream = get_socket_stream('ESP32test')
+            self.input_thread = myThread(1, "Thread-1", 1, self.recv_stream)
+            self.input_thread.start()
             return Builder.load_string(kv)
 
         def send(self, cmd):
             #self.send_stream.write('{}\n'.format(cmd))
-            #self.send_stream.write('{"datum":{"potencia":99.9}')
+            self.send_stream.write("{\"datum\":{\"potencia\":99.9}}\n".encode())
             print(cmd)
             print(self.send_stream)
-            self.send_stream.write(cmd.encode())
+            #self.send_stream.write(cmd.encode())
+            #self.send_stream.write("")
             self.send_stream.flush()
 
         def reset(self, btns):
